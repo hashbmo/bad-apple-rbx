f601f86940c54a8958861283dc9b3a88c1ccdf92
hashbmo <maindami.code@gmail.com>
2022-12-16 02:19:56 +0000

    Update server.py

--- server.py
+++ server.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 # Config
 PORT = 9999
-host = "0.0.0.0"
+HOST = "0.0.0.0"
 
 @app.route("/")
 def home():
     return "<p>Hello<p>"
 
-app.run(host=host,port=PORT)
+app.run(host=HOST,port=PORT)
